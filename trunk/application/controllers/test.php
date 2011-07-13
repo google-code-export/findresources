967429f7337b492eac657a499463b68e5ac70663
Leandro <Leandro@8f7dcdd9-dba8-1640-26e8-43892993bc8c>
2011-07-13 04:26:54 +0000

    armado de controlador para seleccionar que test realizar

--- trunk/application/controllers/test.php
+++ trunk/application/controllers/test.php
@@ -4,43 +4,66 @@ class Test extends CI_Controller {
 	
  	function __construct() {
         parent::__construct();
+        $this->load->library('session');
     }
 
 	function index(){
 		/** REALIZAR VALIDACION DE LOS TESTS A REALIZAR POR EL USUARIO**/
 		/** E IR LLAMANDO A LOS TESTS QUE CORRESPONDAN VALIDANDO CON **/
 		/** LO QUE LE FALTA REALIZAR SEGÚN LA INFO OBTENIDA DE LA BD **/
-		$this->luscher();
+
+		$luscher_state = ($this->session->userdata('luscher')) ? $this->session->userdata('luscher') : "PEND";
+		$d48_state = ($this->session->userdata('d48')) ? $this->session->userdata('d48') : "PEND";
+		$raven_state = ($this->session->userdata('raven')) ? $this->session->userdata('raven') : "PEND";
+		
+		$this->session->set_userdata('luscher', $luscher_state);
+		$this->session->set_userdata('d48', $d48_state);
+		$this->session->set_userdata('raven', $raven_state);
+
+		if ($this->session->userdata('luscher') == "PEND") {
+			$this->luscher("1");
+
+		}else
+		if ($this->session->userdata('d48') == "PEND") {
+			$this->luscher("2");
+
+		} else
+		if ($this->session->userdata('raven') == "PEND") {
+			$this->luscher("3");
+
+		}
 	}
 	
-	function luscher(){
+	function luscher($num){
+		echo $num;
+		$data['num'] = $num;
 		$data['c1'] = '';
 		$data['c2'] = '';
 		$data['timer1'] = '';
 		$data['timer2'] = '';
-		if($this->input->post('init') == '') {
-			$data['source'] = "init_test";
-			$this->load->view('view_test_luscher',$data);
-		 } else {
-		 	if ($this->input->post('colors1') == '') {
+		switch ($this->input->post('source')) {
+			case 'init_test' :
 			 	$data['source'] = "select_colors1";
 			 	$this->load->view('view_test_luscher',$data);
-		 	} else {
-		 		if ($this->input->post('colors2') == '') {
-		 			$data['c1'] = $this->input->post('colors1');
-		 			$data['timer1'] = $this->input->post('timer');
-			 		$data['source'] = "select_colors2";
-			 		$this->load->view('view_test_luscher',$data);
-		 		} else {
-		 			$data['c1'] = $this->input->post('colors1');
-		 			$data['c2'] = $this->input->post('colors2');
-		 			$data['timer1'] = $this->input->post('timer1');
-		 			$data['timer2'] = $this->input->post('timer');
-		 			$data['source'] = "test_finished";
-		 			$this->load->view('view_test_luscher',$data);
-		 		}
-		 	}
-		 }
+			break;
+			case 'select_colors1' :
+		 		$data['c1'] = $this->input->post('colors1');
+		 		$data['timer1'] = $this->input->post('timer');
+			 	$data['source'] = "select_colors2";
+			 	$this->load->view('view_test_luscher',$data);
+			break;
+			case 'select_colors2' : 
+		 		$data['c1'] = $this->input->post('colors1');
+		 		$data['c2'] = $this->input->post('colors2');
+		 		$data['timer1'] = $this->input->post('timer1');
+		 		$data['timer2'] = $this->input->post('timer');
+		 		$data['source'] = "test_finished";
+		 		$this->load->view('view_test_luscher',$data);
+			break;
+			default :
+		 		$data['source'] = "init_test";
+				$this->load->view('view_test_luscher',$data);
+		}
 	}
 
 	
