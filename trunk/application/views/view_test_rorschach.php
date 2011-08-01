0e46d4ac0d0fd6e3677160e70ca483360c2041a5
Leandro <Leandro@8f7dcdd9-dba8-1640-26e8-43892993bc8c>
2011-08-01 04:29:13 +0000

    test de rorschach

---
+++ trunk/application/views/view_test_rorschach.php
@@ -0,0 +1,125 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=Cp1252">
+    <title>FindResources - Test de Rorschach</title> 
+    <meta name="description" content="FindResources - Choose best people" /> 
+    <meta name="keywords" content="personality test images psychology d48 intelligence rorschach" /> 
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.5/jquery.min.js"></script>
+	<script src="<?php echo base_url();?>js/jquery-ui.js" language="JavaScript" type="text/javascript"></script>
+	<script src="<?php echo base_url();?>js/rorschach.js" language="JavaScript" type="text/javascript"></script>
+    <link rel="stylesheet" href="<?php echo base_url();?>css/style.css">
+	<link type="text/css" href="<?php echo base_url();?>css/jquery-ui.css" rel="stylesheet" />
+</head> 
+<body> 
+<?php
+/** PAGINA DE INICIO DEL TEST **/
+if ($source== "init_test"){
+?>
+<div id="page"> 
+<center> 
+<h1 style="text-align:center;">Test de Rorschach</h1> 
+En cada una de las siguientes 10 manchas usted debe indicar qué imágenes vé.
+<br /><br />
+Observe bien las manchas y seleccione el área donde ve una imagen ajustando el tamaño de un recuadro que le aparecerá arriba a la izquierda.
+<br /><br />
+<br /><br />
+<form method="POST" id="quiz_form" name="quiz_form">
+<input type="hidden" name="source" value="init_test" />
+<input type=submit value="Comenzar el test"> 
+</form> 
+</center>
+<?php 
+}
+/** PAGINA DE SELECCION DE FICHAS DEL TEST **/
+if ($source == "select_img"){
+
+if($pic == 10) {	$source = "select_img_final"; }
+?>
+<center><h1>Test de Rorschach</h1>
+<h3>Mancha Nº<?php echo $pic;?></h3>
+<h4>Etiquete la mancha con las imágenes que vé: </h4><br /></center><br /><br />
+<div id="taggingArea">
+	<img src="<?php echo base_url();?>images/rorschach/img<?php echo $pic;?>.jpg" width="800px"/>
+	<div id="drag" class="ui-widget ui-widget-content ui-corner-all">
+   <div class="ui-dialog-titlebar ui-widget-header ui-corner-all ui-helper-clearfix" id="drag_title">
+      <span class="ui-dialog-title">Seleccionar área</span>
+   </div>
+	</div>
+	<?php
+		if(isset($_SESSION["img".$pic]) AND count($_SESSION["img".$pic]) > 0){
+        foreach($_SESSION["img".$pic] as $clave => $resTags) {
+	?>
+				<div class=tag style="position:absolute;width:<?php echo $resTags['width'];?>px;height:<?php echo $resTags['height'];?>px;top:<?php echo $resTags['top'];?>;left:<?php echo $resTags['left'];?>;">
+					<div class="label"><?php echo $resTags['description'];?></div>
+				</div>
+	<?php
+			}
+		}
+	?>
+</div>
+<div id="formArea">
+	<form method=post>
+		<input type=hidden name=pic value="<?php echo $pic;?>">
+		<input type=hidden name=width id=width>
+		<input type=hidden name=height id=height>
+		<input type=hidden name=top id=top>
+		<input type=hidden name=left id=left>
+		Escribe en una palabra lo que vés :<br /><input type=text name=description>
+		<input type=submit value="Etiquetar área">
+		<input type="hidden" name="source" value="<?php echo $source;?>" />
+	</form>
+
+
+	<?php
+	if(isset($_SESSION["img".$pic]) AND count($_SESSION["img".$pic]) > 0){
+       foreach($_SESSION["img".$pic] as $clave => $resTags) {
+       	?>
+		<form method=post>
+		<input type=hidden name=pic value="<?php echo $pic;?>">
+		<input type=hidden name=del value="<?php echo $clave;?>">
+		<input type="hidden" name="source" value="<?php echo $source;?>" />
+		* Etiqueta <?php echo ($clave+1);?> : <?php echo $resTags['description'];?> <input type=submit value="X">
+		</form>
+       	<?php 
+		}
+	}
+	?>
+	<br />
+	<form method=post>
+	<input type=hidden name=pic value="<?php echo $pic+1;?>">
+	<input type="hidden" name="source" value="<?php echo $source;?>" />
+	<input type=submit value="Siguiente imagen >>">
+	</form>
+</div>
+ <?php 
+}
+/** PAGINA FINAL DEL TEST **/
+if ($source == "test_finished") {
+	echo '<br /><h3>Muchas gracias por realizar el Test. Esta información será tenida en cuenta en sus postulaciones.</h3><br /><hr>';
+	echo '<a href="'.base_url().'Test">Continuar con el siguiente test.</a><br /><hr><br />';
+	echo '<pre>RESULTADOS:<br />';
+	
+	print_r($_SESSION);
+echo '</pre>';
+	switch($num){
+		case "1": 
+			$test = "luscher";
+			break;
+		case "2": 
+			$test = "d48";
+			break;
+		case "3": 
+			$test = "raven";
+			break;
+		case "4": 
+			$test = "raven";
+			break;
+	}
+	//$this->session->set_userdata($test, "DONE");
+}
+?>
+</div>
+</body> 
+</html>
